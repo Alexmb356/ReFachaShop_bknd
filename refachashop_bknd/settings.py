b52b51be1a167e5cbe328412f6cac9bb43eb0ca7
alexmb356 <alexmb356@hotmail.com>
2023-12-04 20:20:43 -0300

    Seagrega corsheaders

--- refachashop_bknd/settings.py
+++ refachashop_bknd/settings.py
@@ -35,6 +35,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",    
+    # Otros orígenes permitidos en producción
+]
+
 
 # Application definition
 
@@ -46,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'app',
 ]
 
@@ -57,6 +63,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'refachashop_bknd.urls'
